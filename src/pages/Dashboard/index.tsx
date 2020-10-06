8417d88ada3a39ab47139d163e9b894b075f66ed
Dario gondim <cognusdev@gmail.com>
2020-10-06 17:40:05 -0300

    atualizado ate o fim da aula de estilizaçao da dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
-import { Title } from './styles'
+import { FiChevronRight } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles'
+
+
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github.</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github.</Title>
+
+            <Form action="">
+                <input placeholder="Digite o nome do repositório"></input>
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste" >
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/23459234?s=460&u=424588012c92aad7fa46378a067407b80a7b779d&v=4"
+                        alt="Dario Gondim"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable Reactjs & React Native forms!</p>
+                    </div>
+
+                    <FiChevronRight size={20}></FiChevronRight>
+                </a>
+                <a href="teste" >
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/23459234?s=460&u=424588012c92aad7fa46378a067407b80a7b779d&v=4"
+                        alt="Dario Gondim"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable Reactjs & React Native forms!</p>
+                    </div>
+
+                    <FiChevronRight size={20}></FiChevronRight>
+                </a>
+                <a href="teste" >
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/23459234?s=460&u=424588012c92aad7fa46378a067407b80a7b779d&v=4"
+                        alt="Dario Gondim"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable Reactjs & React Native forms!</p>
+                    </div>
+
+                    <FiChevronRight size={20}></FiChevronRight>
+                </a>
+            </Repositories>
+        </>
+    )
+
+
+
+
 }
 
 export default Dashboard
